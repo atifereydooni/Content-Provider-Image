2a0512a66e6347b6036767b613f154f30a6d14f6
atifereydooni <am.fereydooni@gmail.com>
2023-07-20 22:36:49 +0200

    Implemented List of last 3 days Images from Content Provider

---
+++ app/src/main/java/com/aferi/contentproviderimage/ContentProviderImageViewModel.kt
@@ -0,0 +1,17 @@
+package com.aferi.contentproviderimage
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ContentProviderImageViewModel : ViewModel() {
+
+    var images by mutableStateOf(emptyList<Image>())
+        private set
+
+    fun updateImages(images: List<Image>) {
+        this.images = images
+    }
+
+}
